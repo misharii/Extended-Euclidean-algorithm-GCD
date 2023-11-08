91174200dc00ccf5f24e6cb79535d420c7d20c91
misharii <125268174+misharii@users.noreply.github.com>
2023-11-08 21:51:20 +0300

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -1,1 +1,26 @@
+def gcd(a,b):
+        x=a
+        y=b
+        r=x%y
+        blr=0 # Before Last Remainder
+        l=[]
+        print("Step 1:")
+        while r != 0:
+            blr=r
+            r=x%y
+            q=x//y
+            print("{0} = {1} x {2} + {3}".format(x,q,y,r))
+            x=y
+            y=r
+        print("GCD is {0}".format(blr))
+        print("=========================")
+        print("=== working backwards ===")
+        for i in range :
+            blr =
 
+
+
+
+
+
+gcd(252,198)
